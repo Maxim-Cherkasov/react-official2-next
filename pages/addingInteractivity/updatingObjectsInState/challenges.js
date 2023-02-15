3dfb4099957c9f3916164a90a098e05fe813f4b7
Maxim <84859924+Maxim-Cherkasov@users.noreply.github.com>
2023-02-15 16:25:44 +0300

    task 2

--- pages/addingInteractivity/updatingObjectsInState/challenges.js
+++ pages/addingInteractivity/updatingObjectsInState/challenges.js
@@ -6,6 +6,7 @@
 
 // Docs solution
 
+/*
 import { useState } from "react";
 
 export default function Scoreboard() {
@@ -50,6 +51,50 @@ export default function Scoreboard() {
         Last name:
         <input value={player.lastName} onChange={handleLastNameChange} />
       </label>
+    </>
+  );
+}
+*/
+
+import { useState } from "react";
+import Background from "./Background.js";
+import Box from "./Box.js";
+import style from "./challenges.module.scss";
+
+const initialPosition = {
+  x: 0,
+  y: 0,
+};
+
+export default function Canvas() {
+  const [shape, setShape] = useState({
+    color: "orange",
+    position: initialPosition,
+  });
+
+  function handleMove(dx, dy) {
+    shape.position.x += dx;
+    shape.position.y += dy;
+  }
+
+  function handleColorChange(e) {
+    setShape({
+      ...shape,
+      color: e.target.value,
+    });
+  }
+
+  return (
+    <>
+      <select value={shape.color} onChange={handleColorChange}>
+        <option value="orange">orange</option>
+        <option value="lightpink">lightpink</option>
+        <option value="aliceblue">aliceblue</option>
+      </select>
+      <Background position={initialPosition} />
+      <Box color={shape.color} position={shape.position} onMove={handleMove}>
+        Drag me!
+      </Box>
     </>
   );
 }
