ec578b11c184149d7e08161307a0c7d7bf09279b
Maxim <84859924+Maxim-Cherkasov@users.noreply.github.com>
2023-02-21 18:21:47 +0300

    two sections

--- pages/elemsLib/projectOne/mainSection/callToAction.js
+++ pages/elemsLib/projectOne/mainSection/callToAction.js
@@ -3,7 +3,10 @@ import style from "./callToAction.module.scss";
 export default function CallToAction() {
   return (
     <div className={style.actionContainer}>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+      <p className={style.actionText}>140 машин</p>
+      <p className={style.actionText}>Подача авто 1 час</p>
+      <p className={style.actionText}>480 грузчиков</p>
+      <p className={style.actionText}>Заказ от 490р</p>
       <button type="button">order</button>
     </div>
   );
--- pages/elemsLib/projectOne/mainSection/mainSection.js
+++ pages/elemsLib/projectOne/mainSection/mainSection.js
@@ -21,7 +21,6 @@ export default function MainSection() {
 
       <secction className={style.info}>
         <Playground />
-        <CallToAction />
       </secction>
     </div>
   );
